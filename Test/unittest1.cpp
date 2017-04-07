1ee008ed88a0e70f89bf08d4e4d50bf861693072
Kuzmin Alexey <alexeykuzmin0@gmail.com>
2017-04-07 23:46:06 +0300

    Tuple push added

--- Test/unittest1.cpp
+++ Test/unittest1.cpp
@@ -24,5 +24,17 @@ namespace Test
 				tuple<int, int, string, float>>,
 				"tuple concat test int-int-string-float");
 		}
+
+		TEST_METHOD(TuplePush)
+		{
+			static_assert(is_same_v<
+				tuple_push_t<tuple<int>, string>,
+				tuple<int, string>>,
+				"tuple push test int-string");
+			static_assert(is_same_v<
+				tuple_push_t<tuple<int, int, string>, float>,
+				tuple<int, int, string, float>>,
+				"tuple push test int-int-string-float");
+		}
 	};
 }
