b8fd8a42435f0f24b91877e8e8233641c9510699
Kuzmin Alexey <alexeykuzmin0@gmail.com>
2017-04-08 14:14:55 +0300

    General tree types added

--- StaticTree/tree.h
+++ StaticTree/tree.h
@@ -32,4 +32,18 @@ namespace static_tree
 
     // Empty tree
     struct NIL{};
+
+    // General tree
+    template<class T, class Left, class Right, class Comp>
+    struct node
+    {
+        using type = T;
+        using left = Left;
+        using right = Right;
+        using comp = Comp;
+    };
+
+    // Tree node with no children
+    template <typename T, typename Comp>
+    using leaf = node<T, NIL, NIL, Comp>;
 }
